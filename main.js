44c82f0358e0926b0364796cb1cde79fd5454215
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-03-24 21:03:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+cradles = "";
+toxic = "";
+function preload() {
+    cradles = loadSound("Cradles(PaglaSongs).mp3");
+    toxic = loadSound("BoyWithUke(PaglaSongs).mp3");
+}
 function setup() {
-    canvas = createCanvas(600, 400);
-    canvas.position(325, 170);
+    canvas = createCanvas(500, 400);
+    canvas.position(390, 170);
+    video = createCapture();
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 500, 400);
 }
