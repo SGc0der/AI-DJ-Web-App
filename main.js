a7c22764f62669af190252fb3bff509fd1072c5e
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-03-31 17:46:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 cradles = "";
 toxic = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload() {
     cradles = loadSound("Cradles(PaglaSongs).mp3");
     toxic = loadSound("BoyWithUke(PaglaSongs).mp3");
@@ -9,7 +13,19 @@ function setup() {
     canvas.position(390, 170);
     video = createCapture();
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 function draw() {
     image(video, 0, 0, 500, 400);
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+function gotPoses(results) {
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    rightWristX = results[0].pose.leftWrist.x;
+    rightWristY = results[0].pose.leftWrist.y;
+    console.log("Coordinates: " + leftWristX + ", " + leftWristY + "; " + rightWristX + ", " + rightWristY);
 }
